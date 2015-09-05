732965160a744e9cfabf3e9eff710cfd76b58643
Taavi Teska <taavi@thorgate.eu>
2015-09-05 21:34:28 +0300

    Remove example's Pods dir

--- Example/Pods/Target Support Files/CoreDataManager/CoreDataManager-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CoreDataManager : NSObject
-@end
-@implementation PodsDummy_CoreDataManager
-@end
--- Example/Pods/Target Support Files/Pods-CoreDataManager_Example/Pods-CoreDataManager_Example-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_CoreDataManager_ExampleVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_CoreDataManager_ExampleVersionString[];
-
